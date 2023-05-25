81c0c0de4bc9d54ca2232cccfedae20d3d650229
Olajumoke Fajuyi <olajumokefajuyi0@gmail.com>
2023-05-25 09:16:19 -0700

    simple shell

--- err1.c
+++ err1.c
@@ -2,9 +2,8 @@
 
 /**
  * _erratoi - converts a string to an integer
- * @s: the string to be converted
- * Return: 0 if no numbers in string, converted number otherwise
- *       -1 on error
+ * @s: string to be converted
+ * Return: 0 or  -1 on error
  */
 
 int _erratoi(char *s)
--- err2.c
+++ err2.c
@@ -21,9 +21,7 @@ void _eputs(char *str)
 /**
  * _eputchar - writes the character c to stderr
  * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: On success 1, On error, -1
  */
 
 int _eputchar(char c)
@@ -46,8 +44,7 @@ int _eputchar(char c)
  * @c: The character to print
  * @fd: The filedescriptor to write to
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: On success 1, On error, -1
  */
 
 int _putfd(char c, int fd)
