773ba158b1e78c54ee95c48f18ed3595e4db72d8
Olajumoke Fajuyi <olajumokefajuyi0@gmail.com>
2023-05-25 13:15:56 -0700

    simple shell

--- list_2.c
+++ list_2.c
@@ -8,6 +8,7 @@
  *
  * Return: size of list
  */
+
 list_t *add_node(list_t **head, const char *str, int num)
 {
 	list_t *new_head;
@@ -41,6 +42,7 @@ list_t *add_node(list_t **head, const char *str, int num)
  *
  * Return: size of list
  */
+
 list_t *add_node_end(list_t **head, const char *str, int num)
 {
 	list_t *new_node, *node;
@@ -80,18 +82,19 @@ list_t *add_node_end(list_t **head, const char *str, int num)
  *
  * Return: size of list
  */
+
 size_t print_list_str(const list_t *h)
 {
-	size_t i = 0;
+	size_t x = 0;
 
 	while (h)
 	{
 		_puts(h->str ? h->str : "(nil)");
 		_puts("\n");
 		h = h->next;
-		i++;
+		x++;
 	}
-	return (i);
+	return (x);
 }
 
 /**
@@ -101,10 +104,11 @@ size_t print_list_str(const list_t *h)
  *
  * Return: 1 on success, 0 on failure
  */
+
 int delete_node_at_index(list_t **head, unsigned int index)
 {
 	list_t *node, *prev_node;
-	unsigned int i = 0;
+	unsigned int x = 0;
 
 	if (!head || !*head)
 		return (0);
@@ -120,14 +124,14 @@ int delete_node_at_index(list_t **head, unsigned int index)
 	node = *head;
 	while (node)
 	{
-		if (i == index)
+		if (x == index)
 		{
 			prev_node->next = node->next;
 			free(node->str);
 			free(node);
 			return (1);
 		}
-		i++;
+		x++;
 		prev_node = node;
 		node = node->next;
 	}
@@ -137,9 +141,8 @@ int delete_node_at_index(list_t **head, unsigned int index)
 /**
  * free_list - frees all nodes of a list
  * @head_ptr: address of pointer to head node
- *
- * Return: void
  */
+
 void free_list(list_t **head_ptr)
 {
 	list_t *node, *next_node, *head;
