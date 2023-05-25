6c2d62a89ae9e7486b171a042d9e6203e67dcfab
Olajumoke Fajuyi <olajumokefajuyi0@gmail.com>
2023-05-25 08:43:49 -0700

    simple shell

--- _atoi_.c
+++ _atoi_.c
@@ -1,11 +1,12 @@
 #include "shell.h"
 
 /**
- * interactive - returns true if shell is interactive mode
- * @info: struct address
+ * interactive - function to returns true if shell is interactive mode
+ * @info: structure address
  *
- * Return: 1 if interactive mode, 0 otherwise
+ * Return: 1 or 0
  */
+
 int interactive(info_t *info)
 {
 	return (isatty(STDIN_FILENO) && info->readfd <= 2);
@@ -13,10 +14,11 @@ int interactive(info_t *info)
 
 /**
  * is_delim - checks if character is a delimeter
- * @c: the char to check
+ * @c: the character to check
  * @delim: the delimeter string
- * Return: 1 if true, 0 if false
+ * Return: 1 or 0
  */
+
 int is_delim(char c, char *delim)
 {
 	while (*delim)
@@ -27,8 +29,8 @@ int is_delim(char c, char *delim)
 
 /**
  *_isalpha - checks for alphabetic character
- *@c: The character to input
- *Return: 1 if c is alphabetic, 0 otherwise
+ *@c: character to input
+ *Return: 1 or 0
  */
 
 int _isalpha(int c)
@@ -42,24 +44,24 @@ int _isalpha(int c)
 /**
  *_atoi - converts a string to an integer
  *@s: the string to be converted
- *Return: 0 if no numbers in string, converted number otherwise
+ *Return: 0 or converted number
  */
 
 int _atoi(char *s)
 {
-	int i, sign = 1, flag = 0, output;
+	int x, sign = 1, flag = 0, output;
 	unsigned int result = 0;
 
-	for (i = 0;  s[i] != '\0' && flag != 2; i++)
+	for (x = 0;  s[x] != '\0' && flag != 2; x++)
 	{
-		if (s[i] == '-')
+		if (s[x] == '-')
 			sign *= -1;
 
-		if (s[i] >= '0' && s[i] <= '9')
+		if (s[x] >= '0' && s[x] <= '9')
 		{
 			flag = 1;
 			result *= 10;
-			result += (s[i] - '0');
+			result += (s[x] - '0');
 		}
 		else if (flag == 1)
 			flag = 2;
